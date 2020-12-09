012d5a0bd10e32ed5787809aa7f96493d30f20be
unknown <sk.v9397@gmail.com>
2020-12-10 04:52:30 +0530

    Changes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const marioModel = require('./models/marioChar');
+const marioModel = require("./models/marioChar");
 
 // Middlewares
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // your code goes here
+app.post("/mario", (req, res) => {
+  if (!req.body.weight || req.body.name) {
+    res.status(400).send({ message: "either name or weight is missing" });
+    return;
+  }
+  const mario = new marioModel({
+    name: req.body.name,
+    weight: Number(req.body.weight, 10),
+  });
+  mario
+    .save()
+    .then(() => res.send(mario))
+    .catch((err) => console.logerr);
+});
+app.get("/mario", (req, res) => {
+  marioModel.find({}, (err, result) => {
+    res.send(result);
+  });
+});
+app.get("/mario/:id", (req, res) => {
+  const id = req.params.id;
+  marioModel
+    .findById(id)
+    .then((result) => res.send(result))
+    .catch((err) => res.status(400).send({ message: err.message }));
+});
+app.patch("/mario/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, weight } = req.body;
 
-
-
+  marioModel
+    .findByIdAndUpdate(id, req.body)
+    .then((result) => res.send(result))
+    .catch((error) => res.status(400).send({ message: error.message }));
+});
+app.delete("/mario/:id", (req, res) => {
+  marioModel
+    .findByIdAndDelete(req.params.id)
+    .then((result) => res.send(result))
+    .catch((err) => res.status(400).send({ message: err.message }));
+});
 module.exports = app;
--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //  Your code goes here
-
+const schema = new mongoose.Schema({
+  name: String,
+  weight: Number,
+});
+const marioModel = mongoose.model("mariochar", schema);
 
 module.exports = marioModel;
